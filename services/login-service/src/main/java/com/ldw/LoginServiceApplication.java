941f6166c82dcfe11a9af40852e19ebc73689a78
ldw <2632711107@qq.com>
2025-07-08 00:41:03 +0800

    优化配置文件

--- services/login-service/src/main/java/com/ldw/LoginServiceApplication.java
+++ services/login-service/src/main/java/com/ldw/LoginServiceApplication.java
@@ -2,11 +2,13 @@ package com.ldw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.util.pattern.PatternParseException;
 
 /**
  * @author ldw by 20250701
  */
 @SpringBootApplication
+//@EnableConfigurationProperties(value = PatternParameter.class)
 public class LoginServiceApplication {
 
     public static void main(String[] args) {
