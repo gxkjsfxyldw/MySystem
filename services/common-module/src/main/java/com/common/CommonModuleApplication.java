8eb74a130ef31c023aee46be0f74f8ff2f665b84
lidawang <2632711107@qq.com>
2025-07-23 16:48:05 +0800

    [优化项目结构] 1、升级版本： spring-boot 由2.3.12.RELEASE升级为 2.6.11 spring-cloud 由 Hoxton.SR12 升级为 2021.0.4 spring-cloud-alibaba 由 2.2.9.RELEASE 升级为2021.0.4.0 2、新增依赖： jwt 0.9.0版本 commons-codec 1.15版本 3、在一些依赖的地方加入 scope标签和type

--- services/common-module/src/main/java/com/common/CommonModuleApplication.java
+++ services/common-module/src/main/java/com/common/CommonModuleApplication.java
@@ -1,13 +1,13 @@
-package com.common;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonModuleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CommonModuleApplication.class, args);
-    }
-
-}
+//package com.common;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CommonModuleApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(CommonModuleApplication.class, args);
+//    }
+//
+//}
