2cd0d85b95c0941f2d4fb4da963652305e81f651
ldw <2632711107@qq.com>
2025-07-23 21:47:51 +0800

    【优化项目结构3】确定是nacos-discovery的问题，没有被nacos注册中心发现

--- services/auth-service/src/main/java/com/ldw/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/ldw/AuthServiceApplication.java
@@ -2,12 +2,14 @@ package com.ldw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.web.util.pattern.PatternParseException;
 
 /**
  * @author ldw by 20250701
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 //@EnableConfigurationProperties(value = PatternParameter.class)
 public class AuthServiceApplication {
 
